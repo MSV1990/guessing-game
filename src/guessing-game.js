c4081c435da58db22648726cb3ff73261ec0dd62
msv1990 <htconemailbox@gmail.com>
2019-09-30 16:25:10 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
 
+        const resNumber = Math.ceil((this.max + this.min) / 2);
+        
+        return resNumber;
     }
 
     lower() {
 
+        this.max = Math.ceil((this.max + this.min) / 2);
     }
 
     greater() {
 
+        this.min = Math.ceil((this.max + this.min) / 2);
     }
 }
 
